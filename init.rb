61b24828fb28f392d7e9d776d655f927afeb5ae8
Steffen Roegner <steffen@sroegner.org>
2009-03-14 11:01:22 -0400

    Initial

---
+++ init.rb
@@ -0,0 +1,8 @@
+require 'redmine'
+
+Redmine::Plugin.register :redmine_codebook do
+  name 'Redmine Codebook plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+end
