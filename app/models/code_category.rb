f5a4412dc7a0e099bc23a1d447f7b22e6a89988a
Steffen Roegner <steffen@sroegner.org>
2009-03-15 18:02:13 -0400

    administration for categies is basically working

--- app/models/code_category.rb
+++ app/models/code_category.rb
@@ -3,9 +3,5 @@ class CodeCategory < ActiveRecord::Base
 
   validates_presence_of :name, :type
   validates_uniqueness_of :name, :scope => :type
-  
-  def has_documents?
-    (self.code_documents.empty? == false)
-  end
-  
+    
 end
--- init.rb
+++ init.rb
@@ -1,8 +1,18 @@
 require 'redmine'
 
+#Dir[File.join(directory,'vendor','plugins','*')].each do |dir|
+  #path = File.join(dir, 'lib')
+  #$LOAD_PATH << path
+  #Dependencies.load_paths << path
+  #Dependencies.load_once_paths.delete(path)
+#end
+
 Redmine::Plugin.register :redmine_codebook do
   name 'Redmine Codebook plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Steffen Roegner'
+  author_url 'http://www.sroegner.org'
+  description 'This is a source code sharing plugin for Redmine'
   version '0.0.1'
+  requires_redmine :version_or_higher => '0.8.2'
+  menu(:top_menu, :codebook, {:controller => "codebook", :action => 'index'}, :caption => :codebook_title, :if => Proc.new{ User.current.logged? })
 end
