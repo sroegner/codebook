c7a18db0da45754ee6230515afddbf5f7088ab15
Steffen Roegner <sroegner@sroegner.int.demandware.com>
2009-03-18 10:18:31 -0400

    syntax highlighting added but not complete

--- app/helpers/codebook_helper.rb
+++ app/helpers/codebook_helper.rb
@@ -1,2 +1,20 @@
 module CodebookHelper
+
+  require 'coderay'
+
+  def code_highlight(lang, code)
+    l = lang.downcase
+    all_languages = supported_languages
+    if all_languages.include?(l)
+      return (CodeRay.scan code, l).html(:line_numbers => :inline)
+    else
+      return h(code)
+    end
+  end
+
+  def supported_languages
+    languages = CodeRay::FileType::TypeFromExt.values.collect{|s| s.to_s}
+    languages.uniq!.sort!
+  end
+
 end
