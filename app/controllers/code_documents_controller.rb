a8ec599569cb3d338756bcd8bc8b62ba96ad0310
Steffen Roegner <steffen@sroegner.org>
2009-03-23 06:11:53 -0400

    added filtering tags to index view

--- app/controllers/code_documents_controller.rb
+++ app/controllers/code_documents_controller.rb
@@ -5,6 +5,16 @@ class CodeDocumentsController < ApplicationController
   
   def index
     @code_documents = CodeDocument.find(:all)
+    l = {}
+    a = {}
+    @code_documents.each do |cd|
+      l.key?(cd.code_language.id)? l[cd.code_language.id] += 1 : l[cd.code_language.id] = 1
+      a.key?(cd.code_area.id)? a[cd.code_area.id] += 1 : a[cd.code_area.id] = 1
+    end
+    @used_languages = {"All (#{l.keys.size})" => 0} 
+    CodeLanguage.find(l.keys).collect{|cl| @used_languages["#{cl.name} (#{l[cl.id]})"] = cl.id}.sort
+    @used_areas = {"All (#{a.keys.size})" => 0}
+    CodeArea.find(a.keys).collect{|ca| @used_areas["#{ca.name} (#{a[ca.id]})"]= ca.id}.sort
   end
 
   def new
