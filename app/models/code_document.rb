a9e0fe019b7c2d0d8686fc730f17c8e5885f55b1
Steffen Roegner <steffen@sroegner.org>
2009-03-26 06:46:08 -0400

    added not yet usable search functionality

--- app/models/code_document.rb
+++ app/models/code_document.rb
@@ -4,6 +4,12 @@ class CodeDocument < ActiveRecord::Base
   belongs_to :code_language, :foreign_key => :language_id
   belongs_to :users, :foreign_key => :author_id
   
+  validates_length_of :title, :maximum => 60
+
+  # See http://www.redmine.org/issues/3021
+  # the redmine search can not be used by plugins yet
+  acts_as_searchable :columns => ["title","#{table_name}.description","#{table_name}.code"], :date_column => "created_at"
+
   def authorname(format="short")
     un = User.find(:first, :conditions => ['id=?', self.author_id], :select => "firstname,lastname" )
     format.eql?("short") ? un.firstname : "#{un.firstname} #{un.lastname}"
