e486224dd0ff90be971878498cec5fb88b8ee129
Steffen Roegner <steffen@sroegner.org>
2009-12-01 08:06:17 -0500

    Fixing Issue #3, routing now compatible with new rails and engine versions

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+ActionController::Routing::Routes.draw do |map|
+  map.search '/code_documents/search', :controller => 'code_documents', :action => 'search'
+  map.resources :code_documents
+  connect 'codebook/:action.:format', :controller => 'codebook'
+end
