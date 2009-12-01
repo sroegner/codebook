de87fd57211cf5c8a6d1094d1ab14d4980e6ae78
Steffen Roegner <steffen@sroegner.org>
2009-12-01 08:41:54 -0500

    Issue #3, corrected new routing, tested with Rails 2.3.4

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
   map.search '/code_documents/search', :controller => 'code_documents', :action => 'search'
   map.resources :code_documents
-  connect 'codebook/:action.:format', :controller => 'codebook'
+  map.connect 'codebook/:action.:format', :controller => 'codebook'
 end
