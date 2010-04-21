fcdc07f18782f2ad3e19a69e648e3bf150006d2e
Steffen Roegner <steffen@sroegner.org>
2010-04-21 16:35:34 -0400

    fixed the rspec part

--- spec/models/codebook_documents_spec.rb
+++ spec/models/codebook_documents_spec.rb
@@ -3,7 +3,8 @@ require File.dirname(__FILE__) + '/../spec_helper'
 describe CodeDocument, "the codebook document" do
 
   before(:each) do
-    @doc = mock_model(CodeDocument, :title => "Example Doc", :id => 10)
+    #@doc = mock_model(CodeDocument, :title => "Example Doc", :id => 10)
+    @doc = Factory.build(:code_document)
   end
 
   it "should be happy" do
