5e5a78eb0fb620ac4f794f0c091e453ccd630b54
Steffen Roegner <steffen@sroegner.org>
2010-04-21 16:35:34 -0400

    fixed the rspec part

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,9 +5,13 @@ ENV["RAILS_ENV"] ||= 'test'
 # Allows loading of an environment config based on the environment
 redmine_root = ENV["REDMINE_ROOT"] || File.dirname(__FILE__) + "/../../../.."
 require File.expand_path(redmine_root + "/config/environment")
+require 'ruby-debug'
 require 'spec'
 require 'spec/rails'
-require 'ruby-debug'
+require 'faker'
+require 'factory_girl'
+# Factory Girl doesn't find the factory defs in the default location, give her a hand here
+require File.dirname(__FILE__) + "/factories"
 
 # Uncomment the next line to use webrat's matchers
 #require 'webrat/integrations/rspec-rails'
@@ -17,32 +21,7 @@ require 'ruby-debug'
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
 
 Spec::Runner.configure do |config|
-  # If you're not using ActiveRecord you should remove these
-  # lines, delete config/database.yml and disable :active_record
-  # in your config/boot.rb
   config.use_transactional_fixtures = true
   config.use_instantiated_fixtures  = false
-  config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-
-  # == Fixtures
-  #
-  # You can declare fixtures for each example_group like this:
-  #   describe "...." do
-  #     fixtures :table_a, :table_b
-  #
-  # Alternatively, if you prefer to declare them only once, you can
-  # do so right here. Just uncomment the next line and replace the fixture
-  # names with your fixtures.
-  #
-  # config.global_fixtures = :table_a, :table_b
-  #
-  # If you declare global fixtures, be aware that they will be declared
-  # for all of your examples, even those that don't use them.
-  #
-  # You can also declare which fixtures to use (for example fixtures for test/fixtures):
-  #
-  # config.fixture_path = RAILS_ROOT + '/spec/fixtures/'
-  #
-  # == Mock Framework
   config.mock_with :mocha
 end
